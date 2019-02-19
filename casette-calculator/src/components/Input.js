bc2896ae7ef69984a069086d7828cd8c0dadfe38
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-19 16:25:04 -0600

    General Structure
    Just the basic idea of the components used. One component for each input, (Since it's going to have some dynamic restrictions on input with JS) one component for each row of input (to add more dynamically when they're all filled) and one component for the form overall (potentially to allow tracking multiple casettes).

---
+++ casette-calculator/src/components/Input.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Input extends Component 
+{
+    render()
+    {
+        //todo: make this input only allow numbers, up to 60
+        return(<input type="text"></input>)
+    }
+}
+export default Input;
---
+++ casette-calculator/src/components/InputForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import InputRow from '../components/InputRow.js';
+
+
+class InputForm extends Component 
+{
+    render()
+    {
+        
+        return(
+            <div className="inputform">                
+                <InputRow>
+                
+                </InputRow>
+                <InputRow>
+                
+                </InputRow>
+            </div>
+        )
+    }
+}
+export default InputForm;
