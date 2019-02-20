9c3e4f42aa78186cacc630b9ff0e0a4c26da8f33
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-20 16:36:29 -0600

    Initial Pass
    This has the first part of the intended functionality, only allowing numbers (though it's a bit fiddly). Tomorrow, I'll start only allowing 0-60 as input values.

--- casette-calculator/src/components/Input.js
+++ casette-calculator/src/components/Input.js
@@ -6,19 +6,45 @@ class Input extends Component
         super();
         this.updateState = this.updateState.bind(this);
         this.checkInput = this.checkInput.bind(this);
-        this.state={currentKey:-1};
+        this.state={currentKey:-2};
     }
     checkInput()
     {
-        console.log(this.state.currentKey);
+        if(this.state.currentKey === -2)
+        {
+            console.log("startup");
+        }
+        else if(this.state.currentKey <0)
+        {
+            console.log("incorrect");
+            this.state.eventTarget.value = this.state.eventTarget.value.slice(0,this.state.eventTarget.value.length-1);
+        }
+        else
+        {
+            this.state.eventTarget.value = this.state.eventTarget.value.slice(0,this.state.eventTarget.value.length-1) + this.state.currentKey;
+            console.log(this.state.currentKey);
+        }
     }
     updateState(event)
     {
-        var code = event.keyCode || event.which;
-        //what is this which? Obviously it's saying the keycode or this mysterious which
-        this.setState({
-            currentKey: code
-        });
+        var code = event.key;
+        //so turns out which is the system dependant keycode
+        //but I lean p heavily on us getting event.key's results
+        //so rather than having some unpredicted actions I'm not gonna include which
+        if(code>=0 && code <= 9)
+        {
+            this.setState({
+                currentKey: code,
+                eventTarget: event.target
+            });
+        }
+        else
+        {
+            this.setState({
+                currentKey:-1,
+                eventTarget: event.target
+            });
+        }        
         this.checkInput();
     }
     render()
@@ -27,7 +53,7 @@ class Input extends Component
         //if the js for this is too fiddly/difficult just make a number tag with step of 60
         return(
         <input type="text"
-        onKeyPress={this.updateState.bind(this)}>
+        onKeyDown={this.updateState.bind(this)}>
         </input>)
     }
 }
