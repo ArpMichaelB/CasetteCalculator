e762ae010a7bae0f5ae92995f0a14fa6cb17f3fd
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-21 15:27:24 -0600

    Minutes Fallback
    The minutes now has fallback code that only allows digits, so that input is sanitized as well. Potentially come back to minutes to put a max that's equal to the selected casette length?

--- casette-calculator/src/components/InputMinutes.js
+++ casette-calculator/src/components/InputMinutes.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 class InputMinutes extends Component 
 {
+    constructor(){
+        super();
+        this.increment = this.increment.bind(this);
+    }
+    increment(event)
+    {
+        event.target.value = event.target.value.replace(/[^0-9]/g,"");
+        //so turns out regular expressions are a whole argument type in JS instead of having strings of the given regex
+        //unlike the seconds input, this only needs to be numbers, so we don't need nearly as much bells and whistles in the fallback 
+    }
     render()
     {
         return(
-        <input type="number" min="0">
+        <input type="number" min="0"
+        onChange={this.increment.bind(this)}>
         </input>)
     }
 }
