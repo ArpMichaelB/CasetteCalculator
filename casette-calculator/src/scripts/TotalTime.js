ed46b05fb4265bf01a37287eb2b38848d50806f3
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-22 14:33:03 -0600

    Minor Visual Change
    The display would just display single digit seconds a-la :2 instead of the more-sensible :02, fixed that. For real, ready for dev now. Thank goodness for the magic of regex.

--- casette-calculator/src/scripts/TotalTime.js
+++ casette-calculator/src/scripts/TotalTime.js
@@ -22,9 +22,9 @@ export const totalTime = (event) =>
     totalDisplayer.className = "timeDisplay";
     //TODO: if total time is greater than selected casette, change the class of totalDisplayer so that it's the red^tm
     var totalText = document.createTextNode("Total Time is " + parseInt(totalSeconds/60) + ":" + totalSeconds%60);
-    if(totalText.data.endsWith(":0"))
+    if(totalText.data.search(/:[0-9]$/g) >=0)
     {
-        totalText.data = totalText.data.replace(":0",":00");
+        totalText.data = totalText.data.replace(/:[0-9]$/g,":0" + totalText.data.charAt(totalText.data.length-1));
     }
     totalDisplayer.appendChild(totalText);
     if(form.parentElement.lastChild.className.search("timeDisplay") >= 0)
