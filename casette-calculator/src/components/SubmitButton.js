fac12ab2fd28e06e6761dd33216f20b50f0e9e5c
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-22 14:03:09 -0600

    Intended Functionality
    Adds/changes a text node at the bottom of the page that tells you the total time. This should be all that's required of this branch, so we're able to merge into dev and work on the last initially required feature: the casette length setting.

--- casette-calculator/src/components/SubmitButton.js
+++ casette-calculator/src/components/SubmitButton.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
+import { totalTime } from '../scripts/TotalTime';
 
 class SubmitButton extends Component 
 {
-    totalInputs()
-    {
-        //get the list of everything from the parent element (the input form) and for each row, total the times
-        //then compare it to the total available from lengthpicker
-        //and add a text line to the bottom of the page that says how much time you've used
-        //maybe even add a side marker @half of the total
-        console.log("finish this");
-    }
     render()
     {
         
         return(
             <input type = "button"
             value = "Calculate"
-            onClick={this.totalInputs.bind(this)}>
+            onClick={totalTime}>
             </input>
         )
     }
