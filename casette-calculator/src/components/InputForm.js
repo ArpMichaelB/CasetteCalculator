cd0ac0b3b0daa1e1ea41d368d4cf057e28f19a2a
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-02-22 11:36:31 -0600

    Adding rows, sorta
    The add row button does, in fact, add a row... one time, then never works again. As far as I can tell, the method I'm using isn't duplicating the scripts I've tied to each element. Will work on rebinding those after lunch (might require making a js file that has all my scripts that I pull from, if that's possible that'd be sick)

--- casette-calculator/src/components/InputForm.js
+++ casette-calculator/src/components/InputForm.js
@@ -13,9 +13,6 @@ class InputForm extends Component
                 <InputRow>
                 
                 </InputRow>
-                {/* todo: make it so clicking on the last input row to type makes another input row.
-                 to make this dynamically add rows,  if the second to last child's last child (i.e. the last input seconds) has a value of ""
-                on focus of it, append a child to this element of type input row*/}
                 <SubmitButton>
 
                 </SubmitButton>
--- casette-calculator/src/components/InputRow.js
+++ casette-calculator/src/components/InputRow.js
@@ -5,9 +5,25 @@ import InputMinutes from '../components/InputMinutes.js';
 
 class InputRow extends Component 
 {
+    constructor(){
+        super();
+        this.addRow = this.addRow.bind(this);
+    }
+    addRow(event)
+    {
+        //this following thing is bonkers
+        //event.target.parentElement.parentElement.childNodes[event.target.parentElement.parentElement.childElementCount-2].lastChild
+        //but what it's intended to mean is the last input row on the page's last child (i.e. the add a row button)
+        if(event.target.parentElement.parentElement.childNodes[event.target.parentElement.parentElement.childElementCount-2].lastChild === event.target)
+        {
+            //first of all, add another row
+            event.target.parentElement.parentElement.insertBefore(event.target.parentElement.cloneNode(true),event.target.parentElement.parentElement.lastChild);
+            //then since none of the scripts got copied over, bind them
+            //then, for each row except for the newest one, hide the add a new row button
+        }
+    }
     render()
     {
-        
         return(
             <div className="inputrow">                
                 <InputMinutes className="minute">
@@ -17,6 +33,7 @@ class InputRow extends Component
                 <InputSeconds className="second">
                 
                 </InputSeconds>
+                <input type="button" onClick={this.addRow.bind(this)} className="newRow" value="✓"></input>
             </div>
         )
     }
